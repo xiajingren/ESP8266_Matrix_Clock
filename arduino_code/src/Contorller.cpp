24634c5047dec3c198c3eabfbeb030397c9d784d
xhznl <xhznl@foxmail.com>
2024-11-18 00:13:38 +0800

    ✨ 1

--- arduino_code/src/Contorller.cpp
+++ arduino_code/src/Contorller.cpp
@@ -10,7 +10,9 @@ void Contorller::setup()
 
 void Contorller::loop()
 {
-    displayHumiture();
+    // displayHumiture();
+    displayTime();
+    // displayDate();
 
     // lc.setColumn(0, 0, B00000001);
     // lc.setColumn(1, 1, B01000001);
@@ -42,20 +44,191 @@ void Contorller::displayHumiture()
     float t = -23.60;
     float h = -45.50;
 
-    byte b[100];
-    size_t s = getHumitureContent(t, h, b);
+    byte content[100];
+    size_t size = getHumitureContent(t, h, content);
 
-    position--;
-    if (position < static_cast<int>(0 - s))
-        position = MaxDevices * 8;
+    scrollLeft(content, size);
+}
+
+size_t Contorller::getHumitureContent(float temperature, float humidity, byte *content)
+{
+    size_t index = 0;
+
+    makeNumber(index, content, temperature, 0, 1);
+    makeSpace(index, content);
+    copyToContent(index, content, Fonts_Char1[1], sizeof(Fonts_Char1[1]));
+
+    makeSpace(index, content, 6);
+
+    makeNumber(index, content, humidity, 0, 1);
+    makeSpace(index, content);
+    copyToContent(index, content, Fonts_Char1[2], sizeof(Fonts_Char1[2]));
+
+    return index;
+}
+
+void Contorller::makeSpace(size_t &index, byte *content, size_t count)
+{
+    for (size_t i = 0; i < count; i++)
+    {
+        copyToContent(index, content, Fonts_Char2[0], sizeof(Fonts_Char2[0]));
+    }
+}
+
+void Contorller::displayTime()
+{
+    DateTime dt = DateTime(2024, 1, 1, 12, 9, 20);
+
+    byte content[32];
+    getTimeContent(dt, content);
 
     for (int i = 0; i < MaxDevices; i++)
     {
         for (size_t j = 0; j < 8; j++)
         {
-            if (i * 8 + j - position < s)
+            lc.setColumn(i, j, content[i * 8 + j]);
+        }
+    }
+}
+
+size_t Contorller::getTimeContent(DateTime dt, byte *content)
+{
+    uint8_t h = dt.hour();
+    uint8_t m = dt.minute();
+    uint8_t s = dt.second();
+
+    size_t index = 0;
+
+    makeNumber(index, content, h, 2);
+    makeSpace(index, content);
+
+    copyToContent(index, content, Fonts_Char2[2], sizeof(Fonts_Char2[2]));
+    makeSpace(index, content);
+
+    makeNumber(index, content, m, 2);
+    makeSpace(index, content, 4);
+
+    makeNumber(index, content, s, 2, 0, 1);
+
+    Serial.println(index);
+
+    return index;
+}
+
+void Contorller::displayDate()
+{
+    DateTime dt = DateTime(2024, 1, 2, 12, 10, 20);
+    byte content[100];
+    size_t size = getDateContent(dt, content);
+
+    scrollLeft(content, size);
+}
+
+size_t Contorller::getDateContent(DateTime dt, byte *content)
+{
+    uint16_t y = dt.year();
+    uint8_t m = dt.month();
+    uint8_t d = dt.day();
+
+    uint8_t w = dt.dayOfTheWeek();
+
+    size_t index = 0;
+
+    makeNumber(index, content, y);
+    makeSpace(index, content);
+
+    copyToContent(index, content, Fonts_Char1[0], sizeof(Fonts_Char1[0]));
+    makeSpace(index, content);
+
+    makeNumber(index, content, m, 2);
+    makeSpace(index, content);
+
+    copyToContent(index, content, Fonts_Char1[0], sizeof(Fonts_Char1[0]));
+    makeSpace(index, content);
+
+    makeNumber(index, content, d, 2);
+    makeSpace(index, content, 8);
+
+    switch (w)
+    {
+    case 0:
+        copyToContent(index, content, Fonts_Week_Sun, sizeof(Fonts_Week_Sun));
+        break;
+    case 1:
+        copyToContent(index, content, Fonts_Week_Mon, sizeof(Fonts_Week_Mon));
+        break;
+    case 2:
+        copyToContent(index, content, Fonts_Week_Tue, sizeof(Fonts_Week_Tue));
+        break;
+    case 3:
+        copyToContent(index, content, Fonts_Week_Wed, sizeof(Fonts_Week_Wed));
+        break;
+    case 4:
+        copyToContent(index, content, Fonts_Week_Thu, sizeof(Fonts_Week_Thu));
+        break;
+    case 5:
+        copyToContent(index, content, Fonts_Week_Fri, sizeof(Fonts_Week_Fri));
+        break;
+    case 6:
+        copyToContent(index, content, Fonts_Week_Sat, sizeof(Fonts_Week_Sat));
+        break;
+    default:
+        break;
+    }
+
+    return index;
+}
+
+void Contorller::copyToContent(size_t &index, byte *content, const byte *fonts, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        content[index++] = fonts[i];
+    }
+}
+
+void Contorller::makeNumber(size_t &index, byte *content, float data, signed char width, unsigned char prec, unsigned char numberStyle)
+{
+    char temp[10];
+    sprintf(temp, "%0*.*f", width, prec, data);
+
+    Serial.println("ss1111:" + String(temp));
+
+    for (size_t i = 0; i < sizeof(temp); i++)
+    {
+        if (temp[i] == '\0')
+            break;
+
+        if (i > 0)
+            makeSpace(index, content);
+
+        if (temp[i] == '-')
+        {
+            copyToContent(index, content, Fonts_Char1[0], sizeof(Fonts_Char1[0]));
+        }
+        else if (temp[i] == '.')
+        {
+            copyToContent(index, content, Fonts_Char2[1], sizeof(Fonts_Char2[1]));
+        }
+        else
+        {
+            if (numberStyle == 1)
+                copyToContent(index, content, Fonts_Number2[temp[i] - '0'], sizeof(Fonts_Number2[temp[i] - '0']));
+            else
+                copyToContent(index, content, Fonts_Number1[temp[i] - '0'], sizeof(Fonts_Number1[temp[i] - '0']));
+        }
+    }
+}
+
+void Contorller::scrollLeft(byte *content, size_t size)
+{
+    for (int i = 0; i < MaxDevices; i++)
+    {
+        for (size_t j = 0; j < 8; j++)
+        {
+            if (i * 8 + j - position < size)
             {
-                lc.setColumn(i, j, b[i * 8 + j - position]);
+                lc.setColumn(i, j, content[i * 8 + j - position]);
             }
             else
             {
@@ -63,83 +236,8 @@ void Contorller::displayHumiture()
             }
         }
     }
-}
 
-size_t Contorller::getHumitureContent(float temperature, float humidity, byte *content)
-{
-    size_t index = 0;
-    size_t s = 0;
-    byte temp[50];
-
-    s = getFloatContent(temperature, temp);
-    for (size_t i = 0; i < s; i++)
-    {
-        content[index++] = temp[i];
-    }
-
-    for (size_t j = 0; j < sizeof(NumberFontsV2[11]); j++)
-    {
-        content[index++] = NumberFontsV2[11][j];
-    }
-    index = markSpace(index, content, 6);
-
-    s = getFloatContent(humidity, temp);
-    for (size_t i = 0; i < s; i++)
-    {
-        content[index++] = temp[i];
-    }
-
-    for (size_t j = 0; j < sizeof(NumberFontsV2[12]); j++)
-    {
-        content[index++] = NumberFontsV2[12][j];
-    }
-    index = markSpace(index, content);
-
-    return index;
-}
-
-size_t Contorller::getFloatContent(float data, byte *content)
-{
-    char temp[10];
-    dtostrf(data, 0, 1, temp);
-
-    size_t index = 0;
-    for (size_t i = 0; i < sizeof(temp); i++)
-    {
-        if (temp[i] == '\0')
-            break;
-
-        if (temp[i] == '-')
-        {
-            for (size_t j = 0; j < sizeof(NumberFontsV2[10]); j++)
-            {
-                content[index++] = NumberFontsV2[10][j];
-            }
-            index = markSpace(index, content);
-        }
-        else if (temp[i] == '.')
-        {
-            content[index++] = 1;
-            index = markSpace(index, content);
-        }
-        else
-        {
-            for (size_t j = 0; j < sizeof(NumberFontsV2[temp[i] - '0']); j++)
-            {
-                content[index++] = NumberFontsV2[temp[i] - '0'][j];
-            }
-            index = markSpace(index, content);
-        }
-    }
-
-    return index;
-}
-
-size_t Contorller::markSpace(size_t index, byte *content, size_t length)
-{
-    for (size_t i = 0; i < length; i++)
-    {
-        content[index + i] = 0;
-    }
-    return index + length;
+    position--;
+    if (position < static_cast<int>(0 - size))
+        position = MaxDevices * 8;
 }
--- arduino_code/src/Contorller.h
+++ arduino_code/src/Contorller.h
@@ -2,6 +2,7 @@
 #define Contorller_h
 
 #include "LedControl.h"
+#include "RTClib.h"
 
 class Contorller
 {
@@ -22,8 +23,18 @@ private:
 
     void displayHumiture();
     size_t getHumitureContent(float temperature, float humidity, byte *content);
-    size_t getFloatContent(float data, byte *content);
-    size_t markSpace(size_t index, byte *content, size_t length = 1);
+
+    void displayTime();
+    size_t getTimeContent(DateTime dt, byte *content);
+
+    void displayDate();
+    size_t getDateContent(DateTime dt, byte *content);
+
+    void copyToContent(size_t &index, byte *content, const byte *fonts, size_t size);
+    void makeSpace(size_t &index, byte *content, size_t count = 1);
+    void makeNumber(size_t &index, byte *content, float data, signed char width = 0, unsigned char prec = 0, unsigned char numberStyle = 0);
+
+    void scrollLeft(byte *content, size_t size);
 
 public:
     void setup();
